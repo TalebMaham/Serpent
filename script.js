500b6d5fcbb6a1254f0076fd6f877030dd5a1239
Sidi Mohamed Taleb <sidi.mohamed.taleb@cashflowpositif>
2023-07-07 00:59:33 +0200

    la pomme + serpent mange la pomme + score

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var canvas;
 var xCoord = 0;
 var yCoord = 0;
 var snakee;
+var apple;
+var score = 0 ; 
 
 window.onload = function() {
   init();
@@ -20,33 +22,53 @@ window.onload = function() {
     ctx = canvas.getContext("2d");
 
     snakee = new Snake([[6, 4], [5, 4], [4, 4]], "right");
+    apple = new Apple([10, 10]);
     refreshCanvas();
   }
 
   function refreshCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     snakee.advance();
+
+    if (snakee.checkCollision(apple)) {
+      snakee.grow();
+      apple.move();
+      score ++; 
+    }
+    drawScore(); 
     snakee.draw();
+    apple.draw();
     setTimeout(refreshCanvas, delay);
   }
 
-  function drawBlock(ctx, position) {
-    var x = position[0] * blockSize;
-    var y = position[1] * blockSize;
-    ctx.fillRect(x, y, blockSize, blockSize);
+  function drawScore() {
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Score: " + score, 10, 30);
   }
+  
+function drawBlock(ctx, position, color) {
+  var x = position[0] * blockSize + blockSize / 2;
+  var y = position[1] * blockSize + blockSize / 2;
+  var radius = blockSize / 2;
+
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+  ctx.closePath();
+  ctx.fill();
+}
+
 
   function Snake(body, direction) {
     this.body = body;
     this.currentDirection = direction;
 
     this.draw = function() {
-      ctx.save();
-      ctx.fillStyle = "#ff0000";
       for (var i = 0; i < this.body.length; i++) {
-        drawBlock(ctx, this.body[i]);
+        var color = (i === 0) ? "#ff0000" : "#000000";
+        drawBlock(ctx, this.body[i], color);
       }
-      ctx.restore();
     };
 
     this.advance = function() {
@@ -85,6 +107,39 @@ window.onload = function() {
       if (newDirection !== oppositeDirections[this.currentDirection]) {
         this.currentDirection = newDirection;
       }
+    };
+
+    this.grow = function() {
+      var tail = this.body[this.body.length - 1];
+      this.body.push(tail.slice());
+    };
+
+    this.checkCollision = function(element) {
+      var snakeHead = this.body[0];
+      return snakeHead[0] === element.position[0] && snakeHead[1] === element.position[1];
+    };
+  }
+
+  function Apple(position) {
+    this.position = position;
+
+    this.draw = function() {
+        var x = this.position[0] * blockSize + blockSize / 2;
+        var y = this.position[1] * blockSize + blockSize / 2;
+        var radius = blockSize / 2;
+      
+        ctx.fillStyle = "#00ff00";
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.closePath();
+        ctx.fill();
+      };
+      
+
+    this.move = function() {
+      var randomX = Math.floor(Math.random() * (canvasWidth / blockSize));
+      var randomY = Math.floor(Math.random() * (canvasHeight / blockSize));
+      this.position = [randomX, randomY];
     };
   }
 

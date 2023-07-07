30021d8ec0404255c39e3d7b8deb31a44b897022
Sidi Mohamed Taleb <sidi.mohamed.taleb@cashflowpositif>
2023-07-07 11:49:22 +0200

    serpent avec niveaux de difficulté

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var xCoord = 0;
 var yCoord = 0;
 var snakee;
 var apple;
-var score = 0 ; 
-var gamePaused = false; 
+var score = 0;
+var gamePaused = false;
 
 window.onload = function() {
   init();
@@ -29,42 +29,49 @@ window.onload = function() {
 
   function refreshCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     if (!gamePaused) {
       snakee.advance();
+
       if (snakee.checkCollision(apple)) {
         snakee.grow();
         apple.move();
         score++;
       }
+
+      if (snakee.checkCollisionWithWalls() || snakee.checkCollisionWithSelf()) {
+        gamePaused = true;
+        drawGameOver();
+        return;
+      }
     }
-  
+
     drawScore();
     snakee.draw();
     apple.draw();
+
     if (!gamePaused) {
       setTimeout(refreshCanvas, delay);
     }
   }
-  
 
   function drawScore() {
     ctx.font = "20px Arial";
     ctx.fillStyle = "#000000";
     ctx.fillText("Score: " + score, 10, 30);
   }
-  
-function drawBlock(ctx, position, color) {
-  var x = position[0] * blockSize + blockSize / 2;
-  var y = position[1] * blockSize + blockSize / 2;
-  var radius = blockSize / 2;
 
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-  ctx.closePath();
-  ctx.fill();
-}
+  function drawBlock(ctx, position, color) {
+    var x = position[0] * blockSize + blockSize / 2;
+    var y = position[1] * blockSize + blockSize / 2;
+    var radius = blockSize / 2;
 
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    ctx.closePath();
+    ctx.fill();
+  }
 
   function Snake(body, direction) {
     this.body = body;
@@ -94,9 +101,28 @@ function drawBlock(ctx, position, color) {
           nextPosition[1]++;
           break;
         default:
-          // Cas par défaut si la direction n'est pas reconnue
           break;
       }
+
+      this.checkCollisionWithWalls = function() {
+        var head = this.body[0];
+        var hitLeftWall = head[0] < 0;
+        var hitRightWall = head[0] >= canvasWidth / blockSize;
+        var hitTopWall = head[1] < 0;
+        var hitBottomWall = head[1] >= canvasHeight / blockSize;
+
+        return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
+      };
+
+      this.checkCollisionWithSelf = function() {
+        var head = this.body[0];
+        for (var i = 1; i < this.body.length; i++) {
+          if (head[0] === this.body[i][0] && head[1] === this.body[i][1]) {
+            return true;
+          }
+        }
+        return false;
+      };
 
       this.body.unshift(nextPosition);
       this.body.pop();
@@ -130,17 +156,16 @@ function drawBlock(ctx, position, color) {
     this.position = position;
 
     this.draw = function() {
-        var x = this.position[0] * blockSize + blockSize / 2;
-        var y = this.position[1] * blockSize + blockSize / 2;
-        var radius = blockSize / 2;
-      
-        ctx.fillStyle = "#00ff00";
-        ctx.beginPath();
-        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-        ctx.closePath();
-        ctx.fill();
-      };
-      
+      var x = this.position[0] * blockSize + blockSize / 2;
+      var y = this.position[1] * blockSize + blockSize / 2;
+      var radius = blockSize / 2;
+
+      ctx.fillStyle = "#00ff00";
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+      ctx.closePath();
+      ctx.fill();
+    };
 
     this.move = function() {
       var randomX = Math.floor(Math.random() * (canvasWidth / blockSize));
@@ -152,33 +177,57 @@ function drawBlock(ctx, position, color) {
   document.onkeydown = function handleKeyDown(e) {
     var key = e.keyCode;
     var newDirection;
-  
-    if (key === 32) { // Vérifie si la touche enfoncée est "Espace"
-      gamePaused = !gamePaused; // Inverse l'état de pause
+
+    if (key === 32) {
+      gamePaused = !gamePaused;
       if (!gamePaused) {
-        // Si le jeu est en pause et la touche "Espace" est à nouveau enfoncée, relance le jeu
         refreshCanvas();
       }
     } else {
       switch (key) {
-        case 37: // Flèche gauche
+        case 37:
           newDirection = "left";
           break;
-        case 38: // Flèche haut
+        case 38:
           newDirection = "up";
           break;
-        case 39: // Flèche droite
+        case 39:
           newDirection = "right";
           break;
-        case 40: // Flèche bas
+        case 40:
           newDirection = "down";
           break;
         default:
-          // Autre touche enfoncée, aucune nouvelle direction définie
           break;
       }
       snakee.setDirection(newDirection);
     }
   };
+
+  function drawGameOver() {
+    ctx.font = "40px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Game Over", canvasWidth / 2 - 100, canvasHeight / 2);
+  }
   
+  var buttonFacile = document.createElement('button');
+  buttonFacile.innerHTML = 'Facile';
+  buttonFacile.addEventListener('click', function() {
+    delay = 200;
+  });
+  document.body.appendChild(buttonFacile);
+
+  var buttonMoyenne = document.createElement('button');
+  buttonMoyenne.innerHTML = 'Moyenne';
+  buttonMoyenne.addEventListener('click', function() {
+    delay = 100;
+  });
+  document.body.appendChild(buttonMoyenne);
+
+  var buttonDifficile = document.createElement('button');
+  buttonDifficile.innerHTML = 'Difficile';
+  buttonDifficile.addEventListener('click', function() {
+    delay = 50;
+  });
+  document.body.appendChild(buttonDifficile);
 };
